815458680cafcbe4f32dbda2122dc0e32e3fea4e
Alexis Duran <alexis.duranm@gmail.com>
2021-12-10 14:11:54 +0100

    feat(amplify): add api resource

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "playground": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
